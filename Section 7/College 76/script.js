6596fbcd0b46e2f02bc9761933a0e5c79aef15b2
Seviran <schepens.arnout@gmail.com>
2018-01-05 15:51:27 +0100

    Random Image App

---
+++ Section 7/College 76/script.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector("#btn");
+var img = document.querySelector("#photo");
+
+// Listen for clicks
+btn.addEventListener("click", function() {
+	// Make the request
+	var XHR = new XMLHttpRequest();
+	XHR.onreadystatechange = function() {
+		if (XHR.readyState === 4 && XHR.status === 200) {
+			var url = JSON.parse(XHR.responseText).message;
+			console.log(url);
+			img.src = url;
+		}
+	};
+	XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+	XHR.send();
+});
