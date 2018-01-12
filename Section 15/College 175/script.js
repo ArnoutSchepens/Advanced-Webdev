3b28e5ff351dc42bc2a23cd286101186f52456ed
Seviran <schepens.arnout@gmail.com>
2018-01-12 14:03:10 +0100

    Template Strings

---
+++ Section 15/College 175/script.js
@@ -0,0 +1,12 @@
+var firstName = 'John';
+var lastName = 'Smith';
+
+console.log('Hello ' + firstName + ' ' + lastName);
+console.log(`Hello ${firstName} ${lastName}`);
+console.log(`
+Hello
+How
+Nice
+Is
+This!
+`);
