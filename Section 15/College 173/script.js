af3a0f2e6f6156d4d557fa9de8cca247923fd07e
Seviran <schepens.arnout@gmail.com>
2018-01-12 13:14:25 +0100

    const

---
+++ Section 15/College 173/script.js
@@ -0,0 +1,14 @@
+var first = 'Colt';
+console.log(first);
+first = 'Elie';
+console.log(first);
+
+const second = 'Colt';
+console.log(second);
+// second = "Elie"; // TypeError
+// const second = "Elie" // SyntaxError
+
+const numbers = [1, 2, 3, 4];
+console.log(numbers);
+numbers.push(5);
+console.log(numbers);
