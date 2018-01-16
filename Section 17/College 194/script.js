a35ad47cdf1379580d39f59fb1c211060b24e59d
Seviran <schepens.arnout@gmail.com>
2018-01-16 15:24:27 +0100

    class

---
+++ Section 17/College 194/script.js
@@ -0,0 +1,11 @@
+class Student {
+
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
+
+var alex = new Student('Alex', 'Smith');
+console.log(alex.firstName);
+console.log(alex.lastName);
