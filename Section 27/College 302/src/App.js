7f4b71504956f5d61527d706ad98d64b90100326
Seviran <schepens.arnout@gmail.com>
2018-01-27 16:57:02 +0100

    Update Complex State Solution

---
+++ Section 27/College 302/src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+import { PropTypes } from 'prop-types';
+
+class InstructorItem extends Component {
+	static propTypes = {
+		name: PropTypes.string,
+		hobbies: PropTypes.arrayOf(PropTypes.string)
+	};
+
+	render() {
+		return (
+			<li>
+				<h3>{this.props.name}</h3>
+				<h4>Hobbies: {this.props.hobbies.join(', ')}</h4>
+			</li>
+		);
+	}
+}
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			instructors: [
+				{
+					name: 'Aaron',
+					hobbies: ['Sailing', 'React']
+				},
+				{
+					name: 'Alex',
+					hobbies: ['Math', 'D3']
+				},
+				{
+					name: 'Jonathan',
+					hobbies: ['Css', 'Hiking']
+				},
+				{
+					name: 'Abel',
+					hobbies: ['Music', 'ES2015']
+				}
+			]
+		};
+
+		setTimeout(() => {
+			const randInstructor = Math.floor(
+				Math.random() * this.state.instructors.length
+			);
+
+			const randHobby = Math.floor(
+				Math.random() *
+					this.state.instructors[randInstructor].hobbies.length
+			);
+
+			const instructors = this.state.instructors.map(
+				(instructor, idx) => {
+					if (idx === randInstructor) {
+						const hobbies = [...instructor.hobbies];
+						console.log(hobbies);
+						hobbies.splice(randHobby, 1);
+						return {
+							...instructor,
+							hobbies: hobbies
+						};
+					}
+					return instructor;
+				}
+			);
+
+			this.setState({ instructors });
+		}, 1000);
+	}
+	render() {
+		const instructors = this.state.instructors.map((instructor, idx) => (
+			<InstructorItem 
+			key={idx}
+			name={instructor.name}
+			hobbies={instructor.hobbies}
+			/>
+		));
+		return (
+			<div className="App">
+				<ul>{instructors}</ul>
+			</div>
+		);
+	}
+}
+
+export default App;
